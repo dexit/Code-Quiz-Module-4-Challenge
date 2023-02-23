9d763eacef688e01003e218b92dd7ac04837a28b
dExIT <dexit@dyc.lv>
2023-02-23 23:16:38 +0000

    added base questions

---
+++ js/questions.js
@@ -0,0 +1,33 @@
+var questions = [
+    {
+        title: "Which of the listed options is not a datatype? It is ____",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts",
+        explanation: ""
+    },
+    {
+        title: "When writing a conditional statement ' if / else ' what is used to enclose the logic ? It is enclosed within ____",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses",
+        explanation: ""
+    },
+    {
+        title: "What can you store within Javasscript Arrays. You can use array to store  ____",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above",
+        explanation: ""
+    },
+    {
+        title: "How do you enclose variable strings? Strings need to be enclosed with ____",
+        choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+        answer: "quotes",
+        explanation: ""
+    },
+    {
+        title: "During development, what is the most useful tool that helps the developer to determine code execution success or errors, or any result? You need to use ____",
+        choices: ["Javascript", "terminal / bash", "for loops", "console log"],
+        answer: "console log",
+        explanation: ""
+    },
+
+];
